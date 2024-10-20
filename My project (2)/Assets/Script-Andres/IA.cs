d1f042effdba2fc72f72a2471d93f47c4476d568
Andy-453 <andresfelipe3445@gmail.com>
2024-10-19 21:35:25 -0500

    modif

---
+++ My project (2)/Assets/Script-Andres/IA.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IA : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+
+    public Transform[] destination;
+
+    private int i = 0;
+
+    public float distanceToFollowPath = 2f;
+
+    [Header("-----------Follow------------")]
+
+    public bool followPlayer;
+
+    private GameObject player;
+
+    private float distanceToPlayer;
+
+    public float distanceToFollowPlayer = 10;
+    void Start()
+    {
+        navMeshAgent.destination = destination[i].transform.position;
+        player = FindObjectOfType<Movement>().gameObject;
+    }
+
+    void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        if (distanceToPlayer <= distanceToFollowPlayer)
+        {
+            FollowPlayer();
+        }
+        else
+        {
+            EnemyPath();
+        }
+    }
+
+    public void EnemyPath()
+    {
+        navMeshAgent.destination = destination[i].position;
+        if (Vector3.Distance(transform.position, destination[i].position) <= distanceToFollowPath)
+        {
+            if (destination[i] != destination[destination.Length - 1])
+            {
+                i++;
+            }
+            else
+            {
+                i = 0;
+            }
+        }
+    }
+
+    public void FollowPlayer()
+    {
+        navMeshAgent.destination = player.transform.position;
+    }
+}
--- My project (2)/Assets/Script-Andres/Slot.cs
+++ My project (2)/Assets/Script-Andres/Slot.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Slot : MonoBehaviour
+public class Slot : MonoBehaviour, IPointerClickHandler
 {
     public GameObject item;
     public int ID;
